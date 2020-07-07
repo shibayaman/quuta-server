6a5ba468f7d54fb90a753ba7c2c8049fb2533fd7
shibayaman <shibayaman7374@gmail.com>
2020-07-07 10:41:08 +0900

    Create post & good migrations

---
+++ database/migrations/2020_07_07_004536_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('post_id');
+            $table->string('content');
+            $table->string('user_id');
+            $table->boolean('like_flag');
+            $table->unsignedInteger('good_count');
+            $table->unsignedInteger('comment_count');
+            $table->string('restaurant_id');
+            $table->string('restaurant_name');
+            $table->string('restaurnta_address');
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('user_id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/migrations/2020_07_07_005530_create_goods_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->bigIncrements('good_id');
+            $table->unsignedBigInteger('post_id');
+            $table->string('user_id');
+            $table->foreign('post_id')
+                ->references('post_id')->on('posts')
+                ->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('user_id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
