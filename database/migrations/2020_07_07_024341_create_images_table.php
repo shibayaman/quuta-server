c85c7bea30b66fc67cc058baa88e22c7b447679a
shibayaman <shibayaman7374@gmail.com>
2020-07-07 11:46:37 +0900

    Create images migration

---
+++ database/migrations/2020_07_07_024341_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('image_id');
+            $table->unsignedBigInteger('post_id');
+            $table->string('image_url');
+            $table->string('dish_name');
+            $table->foreign('post_id')
+                ->references('post_id')->on('posts')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
