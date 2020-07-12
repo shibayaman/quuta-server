602b1dc2bd92fc8d0b001968e63b8a8b6ba9bb37
shibayaman <shibayaman7374@gmail.com>
2020-07-12 23:38:38 +0900

    Add Follow factory & seeder

---
+++ database/seeds/FollowSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Follow;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class FollowSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = User::all();
+
+        $users->each(function ($user) use ($users) {
+            $followings = $users->except([$user->user_id])->random(4);
+
+            $followings->each(function ($following) use ($user) {
+                $user->following()->save(
+                    factory(Follow::class)->make(
+                        [
+                        'follow_user_id' => $following->user_id
+                    ]
+                    )
+                );
+            });
+        });
+    }
+}
