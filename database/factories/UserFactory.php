4e5c34a96c53b5788cbf1b803265be6da4517845
shibayaman <shibayaman7374@gmail.com>
2020-07-12 23:27:30 +0900

    Add User factory & seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,26 +3,27 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
+use App\Sex;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(User::class, function (Faker $faker) {
+    $sex = Sex::all();
+
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'user_id' => Str::random(7),
+        'username' => $faker->username,
+        'email' => $faker->unique()->email,
+        'password' => Hash::make(Str::random(10)),
+        'password_updated_at' => null,
+        'private_flag' => false,
+        'birthday_date' => $faker->date('Y-m-d', 'now'),
+        'sex_id' => $sex->isNotEmpty() ? $sex->random()->sex_id : null,
+        'icon_url' => 'http://placecorgi.com/100/100',
+        'password_reset_token' => null,
+        'token_expires_at' => null,
+        'description' => $faker->sentence(3),
     ];
 });
