d5923456cff7986d14b0242496264659c484ceb7
shibayaman <shibayaman7374@gmail.com>
2020-07-13 03:04:16 +0900

    Add all the seeding to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,16 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(SexSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(ImageSeeder::class);
+        $this->call(FollowSeeder::class);
+        $this->call(GoodSeeder::class);
+        $this->call(ThreadSeeder::class);
+        $this->call(CommentSeeder::class);
     }
 }
