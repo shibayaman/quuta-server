384c18b726e57ccb821c9d9997ecf456d1aa05d8
shibayaman <shibayaman7374@gmail.com>
2020-08-18 07:36:36 +0900

    Add スレッド親コメント作成のテスト

---
+++ tests/Feature/ParentCommentTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Comment;
+use App\Post;
+use App\Thread;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use UserPostSeeder;
+use Tests\TestCase;
+
+class ParentCommentTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function itSavesThreadsAndComment()
+    {
+        $this->seed(UserPostSeeder::class);
+        $user = User::first();
+        $post = Post::first();
+
+        $commentContent = 'random comment';
+        $response = $this->actingAs($user)->postJson('/api/comment/parent', [
+            'post_id' => $post->post_id,
+            'comment' => $commentContent
+        ]);
+
+        $response->assertStatus(201);
+        
+        $thread = Thread::all();
+        $this->assertEquals($thread->count(), 1);
+        $this->assertEquals($thread[0]->post_id, $post->post_id);
+
+        $comment = Comment::find($thread[0]->comment_id);
+        $this->assertEquals($comment->user_id, $user->user_id);
+        $this->assertEquals($comment->comment, $commentContent);
+    }
+}
