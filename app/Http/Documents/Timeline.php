1e37c2a54f3537f838805f3b9e6a99d70b650117
shibayaman <shibayaman7374@gmail.com>
2020-11-25 19:38:31 +0900

    Add タイムライン取得のドキュメント

---
+++ app/Http/Documents/Timeline.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+* @OA\Parameter(
+*   parameter="timeline_get_since_id",
+*   name="since_id",
+*   example="1",
+*   in="query",
+*   description="投稿のid、これ以降に投稿された投稿を取得する",
+* ),
+*/
+
+/**
+* @OA\Parameter(
+*   parameter="timeline_get_until_id",
+*   name="until_id",
+*   example="30",
+*   in="query",
+*   description="投稿のid、これ以前に投稿された投稿を取得する",
+* ),
+*/
+
+/**
+* @OA\Parameter(
+*   parameter="timeline_get_count",
+*   name="count",
+*   example="20",
+*   in="query",
+*   description="取得する投稿の数、 デフォルト10、最大50",
+* ),
+*/
+
+/**
+* @OA\Parameter(
+*   parameter="timeline_get_user_id",
+*   name="user_id",
+*   in="query",
+*   required=true,
+*   description="取得するユーザのid",
+* ),
+*/
