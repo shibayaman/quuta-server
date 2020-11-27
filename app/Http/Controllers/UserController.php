9e953fca13dadd7e707c424eecbc9fd9894273f1
shibayaman <shibayaman7374@gmail.com>
2020-11-27 18:58:27 +0900

    Add ユーザ取得のドキュメント

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,30 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * @OA\Get(
+     *  path="/api/user/{id}",
+     *  summary="ユーザ情報取得",
+     *  description="ユーザの登録情報を取得する",
+     *  operationId="getUserInfo",
+     *  tags={"user"},
+     *  security={{"bearer": {}}},
+     *  @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      description="取得したいユーザのID",
+     *  ),
+     *  @OA\Response(
+     *      response=404,
+     *      description="指定されたユーザが存在しない",
+     *  ),
+     *  @OA\Response(
+     *      response=200,
+     *      description="Success",
+     *      @OA\MediaType(mediaType="application/json")
+     *  ),
+     * )
+     */
     public function show(User $user)
     {
         if (Auth::check()) {
